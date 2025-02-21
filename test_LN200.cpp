74b0b05588f02d17ffd20c8261199ce3b1d0d3e3
Eric Mehiel <emehiel@calpoly.edu>
2025-02-21 14:35:05 -0800

    Update test_LN200.cpp

--- test_LN200.cpp
+++ test_LN200.cpp
@@ -10,16 +10,17 @@
 
 using namespace std;
 
-int main(void)
+int main(int argc, char *argv[])
 {
     LN200* myLN200 = new LN200();
 
     cout << "Hello Eric, how's the IMU?" << endl;
-    ofstream myFile;
-    myFile.open("LN200_data.txt");
+    
     Timer t; 
     
-    int n = 5000;
+    int sampleRate = 5; // Hz
+    int sampelTime = 30; // seconds
+    int n = sampleRate*sampelTime;
     double ax[n] {0};
     double ay[n] {0};
     double az[n] {0};
@@ -31,6 +32,8 @@ int main(void)
     for (int i = 0; i < n; i++)
     {
         t.reset();
+        t.pause(200);
+        myLN200->ReadIMU();
         ax[i] =  myLN200->GetAccelX();
         ay[i] =  myLN200->GetAccelY();
         az[i] =  myLN200->GetAccelZ();
@@ -39,17 +42,15 @@ int main(void)
         oy[i] =  myLN200->GetOmegaY();
         oz[i] =  myLN200->GetOmegaZ();
         dt[i] = t.elapsed();
+        cout << dt[i] << endl;
     }
+
+    ofstream myFile;
+    myFile.open("LN200_data.txt");
 
     for (int i = 0; i < n; i++)
     {
-        myFile << ax[i] << "\t";
-        myFile << ay[i] << "\t";
-        myFile << az[i] << "\t";
-        myFile << ox[i] << "\t";
-        myFile << oy[i] << "\t";
-        myFile << oz[i] << "\t";
-        myFile << dt[i] << endl;
+        myFile << ax[i] << "\t" << ay[i] << "\t" << az[i] << "\t" << ox[i] << "\t" << oy[i] << "\t" << oz[i] << "\t" << dt[i] << endl;
     }
 
     myFile.close();
